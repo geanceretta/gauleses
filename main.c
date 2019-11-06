b67f2287cb9c21b0f1449c31a958c1586e660af3
Gean Michel Ceretta <geanceretta@gmail.com>
2019-11-06 16:01:46 -0300

    fix semaphores logic

--- main.c
+++ main.c
@@ -4,92 +4,103 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-sem_t *mutex, *sem_empty, *sem_full;
+sem_t *mutex, *sem_full, *sem_cozinhar;
 int javalis_grelhados = 0;
 
 //TODO generic process receiving a struct as parameter, containing process_name and a list of semaphores to wait and to post
 
 void* process_g(void* unused){
     while(1){
-        sem_wait(sem_full);
         sem_wait(mutex);
-        javalis_grelhados--;
         if (javalis_grelhados == 0) {
             printf("Javali g(0) acordou o cozinheiro\n");
+            sem_post(sem_cozinhar);
+            sem_wait(sem_full);
         }
+        printf("Gaules g(0) comendo javali %d\n", javalis_grelhados);
+        javalis_grelhados--;
         sem_post(mutex);
-        sem_post(sem_empty);
-        printf("Javali g(0) comendo\n");
-        sleep((rand()%4)+1);
+        //sleep((rand()%4)+1);
     }
     pthread_exit(NULL);
 }
 
 void* process_e(void* unused){
     while(1){
-        sem_wait(sem_full);
         sem_wait(mutex);
-        javalis_grelhados--;
         if (javalis_grelhados == 0) {
             printf("Javali e(1) acordou o cozinheiro\n");
+            sem_post(sem_cozinhar);
+            sem_wait(sem_full);
         }
+        printf("Gaules e(1) comendo javali %d\n", javalis_grelhados);
+        javalis_grelhados--;
         sem_post(mutex);
-        sem_post(sem_empty);
-        printf("Javali e(1) comendo\n");
-        sleep((rand()%4)+1);
+        //sleep((rand()%4)+1);
     }
     pthread_exit(NULL);
 }
 
 void* process_a(void* unused){
     while(1){
-        sem_wait(sem_full);
         sem_wait(mutex);
-        javalis_grelhados--;
-        if (javalis_grelhados == 0) {
+        if (javalis_grelhados < 1) {
             printf("Javali a(2) acordou o cozinheiro\n");
+            sem_post(sem_cozinhar);
+            sem_wait(sem_full);
         }
+        printf("Gaules a(2) comendo javali %d\n", javalis_grelhados);
+        javalis_grelhados--;
         sem_post(mutex);
-        sem_post(sem_empty);
-        printf("Javali a(2) comendo\n");
-        sleep((rand()%4)+1);
+        //sleep((rand()%4)+1);
     }
     pthread_exit(NULL);
 }
 
 void* process_n(void* unused){
     while(1){
-        sem_wait(sem_full);
         sem_wait(mutex);
-        javalis_grelhados--;
-        if (javalis_grelhados == 0) {
+        if (javalis_grelhados < 1) {
             printf("Javali n(3) acordou o cozinheiro\n");
+            sem_post(sem_cozinhar);
+            sem_wait(sem_full);
         }
+        printf("Gaules n(3) comendo javali %d\n", javalis_grelhados);
+        javalis_grelhados--;
+        //sleep((rand()%4)+1);
         sem_post(mutex);
-        sem_post(sem_empty);
-        printf("Javali a(3) comendo\n");
-        sleep((rand()%4)+1);
     }
     pthread_exit(NULL);
 }
 
 void* process_c(void* unused){
     while(1){
-        sleep((rand()%4)+1);
-        sem_wait(sem_empty);
-        sem_wait(mutex);
-        javalis_grelhados++;
-        sem_post(mutex);
+        sem_wait(sem_cozinhar);
+        while(javalis_grelhados < 10){
+            //sleep((rand()%4)+1);
+            javalis_grelhados++;
+            printf("cozinheiro grelhando javali %d...\n", javalis_grelhados);
+        }
         sem_post(sem_full);
     }
     pthread_exit(NULL);
 }
 
 int main (void){
+    // Close previous semaphores
+    sem_close(mutex);
+    sem_close(sem_full);
+    sem_close(sem_cozinhar);
+    
+    // Unlink previous named semaphores
+    sem_unlink("/mutex");
+    sem_unlink("/sem_full");
+    sem_unlink("/sem_cozinhar");
+
     // Semaphores initialization
     mutex = sem_open("/mutex", O_CREAT, 0644, 1);
-    sem_empty = sem_open("/sem_empty", O_CREAT, 0644, 10);
     sem_full = sem_open("/sem_full", O_CREAT, 0644, 0);
+    sem_cozinhar = sem_open("/sem_cozinhar", O_CREAT, 0644, 0);
 
     // Threads creation
     pthread_t thread_g, thread_e, thread_a, thread_n, thread_c;
@@ -108,11 +119,11 @@ int main (void){
     
     // Close named semaphores
     sem_close(mutex);
-    sem_close(sem_empty);
     sem_close(sem_full);
+    sem_close(sem_cozinhar);
     
     // Unlink named semaphores
     sem_unlink("/mutex");
-    sem_unlink("/sem_empty");
     sem_unlink("/sem_full");
+    sem_unlink("/sem_cozinhar");
 }
